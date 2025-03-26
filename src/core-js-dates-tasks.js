2acb72ab891fb3850f483162f1e0a3cd6dddd6de
RamanMilashevich <roma.milashevich@gmail.com>
2025-03-26 18:40:17 +0100

    feat: add solution to getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -63,8 +63,15 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const data = new Date(date);
+  const day = data.getDay();
+  if (day === 5) {
+    data.setDate(data.getDate() + 7);
+  } else {
+    data.setDate(data.getDate() + ((5 - day + 7) % 7));
+  }
+  return data;
 }
 
 /**
